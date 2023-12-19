02ab2c72a3ab9d3ee189295466171be4f84e6bd7
Victorospi <victorospi30@gmail.com>
2023-12-19 22:30:58 +0200

    3-rd upload after fix

--- automation_test.py
+++ automation_test.py
@@ -5,8 +5,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-service = Service(executable_path='C:/Users/Osipov/Desktop/Repos_1/Victor/chromedriver_linux64/chromedriver')
-driver = webdriver.Chrome(service=service)
+options = webdriver.ChromeOptions()
+service = Service(executable_path='C:/Users/Osipov/Desktop/Repos_1/Victor/chromedriver_linux64/chromedriver.exe')
+driver = webdriver.Chrome(service=service, options=options)
 
 try:
     driver.maximize_window()
