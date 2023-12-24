decf607b7fba1fab3ff211dd68d6467ac191a8a8
Victorospi <victorospi30@gmail.com>
2023-12-24 23:31:31 +0200

    automation file open VK

--- automation_test.py
+++ automation_test.py
@@ -11,7 +11,22 @@ driver = webdriver.Chrome(service=service, options=options)
 
 try:
     driver.maximize_window()
-    driver.get('https://google.com')
+    driver.get('https://vk.com')
+    time.sleep(3)
+
+    email_input = driver.find_element(By.ID, 'index_email')
+    email_input.clear()
+    email_input.send_keys('victorospi@mail.ru')
+    email_input.send_keys(Keys.ENTER)
+    time.sleep(5)
+
+    password_input = driver.find_element(By.NAME, 'password')
+    password_input.clear()
+    password_input.send_keys('Kasha11!')
+    password_input.send_keys(Keys.ENTER)
+    time.sleep(10)
+
+    my_page = driver.find_element(By.PARTIAL_LINK_TEXT, 'My profile').click()
     time.sleep(5)
 except Exception as ex:
     print(ex)
