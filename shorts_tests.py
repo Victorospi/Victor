cadd7c7231891e029a24b31c10393f7ca611fe15
Victor <victorospi30@gmail.com>
2025-05-14 17:09:25 +0300

    changed shorts_tests.py file

--- shorts_tests.py
+++ shorts_tests.py
@@ -1,5 +1,6 @@
 print("hello world")
 
+
 #Find number in string, and print result string withot it
 x = (2,3,4,2,5)
 for i in x:
@@ -7,17 +8,20 @@ for i in x:
     print(i, end=" ")
 print("")
 
+
 #Print string with simmilar numbers in two different strings
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 result = [elem for elem in a if elem in b]
 print (result)
 
+
 #Print sum of digits in number
 def sum_digits(num):
   digits = [int(i) for i in str(num)]
   return sum(digits)
 print(sum_digits(1234))
+
 
 #Print longest and frequent word in string
 import collections
@@ -27,6 +31,7 @@ counter = collections.Counter(words)
 mostWord, occurrences = counter.most_common()[0]
 longestWord = max(words, key=len)
 print(mostWord, longestWord)
+
 
 #Reverse inputed number and print it
 n1 = input("Введите целое число: ")
