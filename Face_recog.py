82dc1c62f4f3153814fda58463db34aa924e5747
Victor <victorospi30@gmail.com>
2025-07-08 16:49:23 +0300

    working on face_recog

--- Face_recog.py
+++ Face_recog.py
@@ -2,5 +2,13 @@ from ultralytics import YOLO
 from collections import Counter
 from pathlib import Path
 
+def detect_objects(model: YOLO, image_pass: str) -> None:
+    ''' Peform detecting, save in to file'''
+    pass
 
+def main():
+    pass
+
+if __name__ == '__main__':
+   main()
 
