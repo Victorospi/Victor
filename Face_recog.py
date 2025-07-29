bbf7ae3f2864e602525861c1b47a6027cb9bb735
Victor <victorospi30@gmail.com>
2025-07-29 13:09:37 +0300

    promote work on Face_recog

--- Face_recog.py
+++ Face_recog.py
@@ -3,11 +3,20 @@ from collections import Counter
 from pathlib import Path
 
 def detect_objects(model: YOLO, image_pass: str) -> None:
-    ''' Peform detecting, save in to file'''
-    pass
+    ''' 
+    Peform detecting, save in to file
+    param - model - Downloaded YOLO model
+    param - image_pass - Way to image for detection
+    '''
+    print("[INFO] start objects recognize...")
+    results = model(image_pass, verbose=False)[0]
+
+    print(results)
 
 def main():
-    pass
+    model = YOLO('yolov8n.pt')
+
+    detect_objects(model,'\Victor_Examples\images\ImgExample_footbalTeam.jpg')
 
 if __name__ == '__main__':
    main()
