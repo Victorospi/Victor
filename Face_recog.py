ac85a58d84cef1cba2a05d8717a321fa2130236f
Victor <victorospi30@gmail.com>
2025-08-14 11:29:24 +0300

    finish work on face_recog

--- Face_recog.py
+++ Face_recog.py
@@ -4,19 +4,31 @@ from pathlib import Path
 
 def detect_objects(model: YOLO, image_pass: str) -> None:
     ''' 
-    Peform detecting, save in to file
-    param - model - Downloaded YOLO model
-    param - image_pass - Way to image for detection
+    Peform object detecting
+    param model - Downloaded YOLO model
+    param image_pass - Way to image for detection
     '''
     print("[INFO] start objects recognize...")
     results = model(image_pass, verbose=False)[0]
 
-    print(results)
+    if results.names and results.boxes is not None:
+        #get classify for object classes
+        labels = results.boxes.cls.tolist()
+        #change ID to class names
+        label_names = [results.names[int(cls)] for cls in labels]
+        #count each unic object
+        counts = Counter(label_names)
+
+        print("[INFO] objects recognized:")
+        for label, count in counts.items():
+            print(f'[+] {label}: {count}')
+    else:
+        print('[!] objects not recognized')
 
 def main():
     model = YOLO('yolov8n.pt')
 
-    detect_objects(model,'\images\ImgExample_footbalTeam.jpg')
+    detect_objects(model,'\images\img_exmpl_1.jpg')
 
 if __name__ == '__main__':
    main()
